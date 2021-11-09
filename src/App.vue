612fbaba9f2acbea81d836584040a98706d970a3
ErickEscagion <erickeescagion@gmail.com>
2021-11-09 17:26:39 -0300

    site v1

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,87 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-system-bar color="blue darken-4"></v-system-bar>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="blue--text text--lighten-1"
+        >
+          <v-list-item to="/">
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
 
-      <v-spacer></v-spacer>
+          <v-list-item to="/About">
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+          <v-list-item to="/Test">
+            <v-list-item-title>Pokemons</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar color="blue lighten-1" dark max-height="60">
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My site Vue</v-toolbar-title>
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
     </v-main>
+
+    <v-footer color="blue darken-4" dark>
+      <v-col class="text-center" cols="12">
+        <strong>Erick Escagion</strong> @ {{ new Date().getFullYear() }}
+      </v-col>
+    </v-footer>
+
+    <v-bottom-navigation
+      grow
+      :value="value"
+      color="blue"
+      max-height="10vh"
+      background-color="#f5f5f5"
+    >
+      <v-btn>
+        <span>Icone 1</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Icone 2</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Icone 3</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    //
+    value: 1,
+    drawer: false,
+    group: null,
   }),
+
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
 };
 </script>
