67729863efa7d070647c5bfc33a5e10abfee6f49
ErickEscagion <erickeescagion@gmail.com>
2021-11-23 10:39:11 -0300

    new HomePage

---
+++ src/components/HomePage.vue
@@ -0,0 +1,32 @@
+
+<template >
+  <div >
+    <v-card elevation="2" align="center">
+      <v-img
+        max-height="850"
+        src="../assets/poke.png"
+      ></v-img>
+
+      <p>
+        Pokémon são criaturas fictícias que pertencem ao universo da série de
+      mesmo nome - Pokémon. Originalmente, a série foi criada como um jogo de
+      videogame e, com a sua popularização, se espalhou para diversos outros
+      formatos, como séries de TV, filmes e livros.<br /><br />
+      A palavra pokémon é a contração de duas palavras em inglês: pocket, que
+      significa bolso; e monster, que significa monstro. Assim, um pokémon é um
+      "monstro de bolso", na tradução literal.<br /><br />
+      Os jogos do gênero RPG da franquia Pokémon foram criados por Satoshi
+      Tajiri em 1996. Com o sucesso do jogo, houve a adaptação para formatos de
+      animação audiovisual, como animes (desenhos animados japoneses) e
+      filmes.<br /><br />
+      A maior parte dos pokémon sofrem metamorfoses, evoluindo para seres mais
+      avançados e com uma estética diferente. Com o tempo também aprendem novas
+      técnicas ou ataques, que podem utilizar em combate.<br /><br />
+      Com o sucesso do anime no final da década de 1990, surgiram vários outros
+      jogos de Pokémon, os primeiros para console Game Boy, da empresa Nintendo.
+      Mais tarde, foram criados jogos online de Pokémon, como o Pokémon Deluge e
+      o Pokémon GO.<br />
+      </p>
+    </v-card>
+  </div>
+</template>
